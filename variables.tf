86a71622bd111a6a518477847c76a1816f3d18fb
Deserie Murembeni <deserie.dev@gmail.com>
2022-03-12 20:18:56 +0200

    provision vpc, add default tags

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  type        = string
+  description = "AWS region where all resources will be created"
+  default     = "af-south-1"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for VPC"
+  default     = "10.255.0.0/20"
+}
+
+variable "default_tags" {
+  type        = map(string)
+  description = "Map of default tags to assign to resources"
+  default = {
+    "project" = "Deserie: AWS & HashiCorp Microservice"
+  }
+}
