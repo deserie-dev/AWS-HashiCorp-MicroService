026c89c35f2e04fad2d26fee7dbb013804c4846e
Deserie Murembeni <deserie.dev@gmail.com>
2022-03-17 13:08:37 +0200

    private subnet, nat, route table

--- variables.tf
+++ variables.tf
@@ -17,3 +17,15 @@ variable "default_tags" {
     "project" = "Deserie: AWS & HashiCorp Microservice"
   }
 }
+
+variable "public_subnet_count" {
+  type        = number
+  description = "Number of public subnets to create"
+  default     = 2
+}
+
+variable "private_subnet_count" {
+  type        = number
+  description = "Number of private subnets to create"
+  default     = 2
+}
